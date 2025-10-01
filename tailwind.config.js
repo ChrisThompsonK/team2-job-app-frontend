afedaf0c3d28adbc6c5848301743eee533835f83
ryanmagee-kainos <ryan.magee@kainos.com>
2025-10-01 12:23:48 +0100

    adding tailwind css (#10)

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,ts,njk}",
+    "./src/views/**/*.njk"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
