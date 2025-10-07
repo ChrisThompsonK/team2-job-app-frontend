d45466a1c1ccd5e521c9f4388b30e372f3aea697
FilipKainos <filip.szwerluga@kainos.com>
2025-10-07 04:31:31 -0700

    Biome Check

--- src/test/axios.test.ts
+++ src/test/axios.test.ts
@@ -1,9 +1,10 @@
-import { api } from '../config/api';
+import { api } from "../config/api";
 
-api.get('/test-endpoint')
-  .then((response) => {
-    console.log('API Response:', response.data);
-  })
-  .catch((error) => {
-    console.error('API Error:', error);
-  });
+api
+	.get("/test-endpoint")
+	.then((response) => {
+		console.log("API Response:", response.data);
+	})
+	.catch((error) => {
+		console.error("API Error:", error);
+	});
