b6e75779ef70e0e0cd99ca233dac232ae76823ca
FilipKainos <filip.szwerluga@kainos.com>
2025-11-19 10:31:39 +0000

    Fix Terraform apply: add import block for existing resources and improve error handling

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,8 @@
+import {
+  to = azurerm_resource_group.rg
+  id = "/subscriptions/ef1b41f6-e4b2-41d0-a52d-d279af4a77ab/resourceGroups/team2-fs-test-rg-dev-rg"
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
