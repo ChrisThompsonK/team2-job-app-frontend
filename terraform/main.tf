d5b0f64bf004d6663c7d9d18a0a84fcf05b687b3
Filip Szwerluga <filip.szwerluga@kainos.com>
2025-11-20 10:18:08 +0000

    Managed Identify Frontend

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,29 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# User-Assigned Managed Identity for Container App
+resource "azurerm_user_assigned_identity" "container_identity" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.main.name
+  location            = azurerm_resource_group.main.location
+
+  tags = {
+    Environment = var.environment
+    Application = var.app_name
+  }
+}
+
+# Role Assignment: Grant AcrPull to Managed Identity
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.container_identity.principal_id
+}
+
+# Role Assignment: Grant Key Vault Secrets User to Managed Identity
+resource "azurerm_role_assignment" "kv_secrets_user" {
+  scope                = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.container_identity.principal_id
+}
