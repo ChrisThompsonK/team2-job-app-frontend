79d5e04f5d1acfbebe7f4cb7ec3b712f089ab2a1
Filip Szwerluga <filip.szwerluga@kainos.com>
2025-11-20 10:25:28 +0000

    main -> rg

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,8 +6,8 @@ resource "azurerm_resource_group" "rg" {
 # User-Assigned Managed Identity for Container App
 resource "azurerm_user_assigned_identity" "container_identity" {
   name                = var.app_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
   tags = {
     Environment = var.environment
