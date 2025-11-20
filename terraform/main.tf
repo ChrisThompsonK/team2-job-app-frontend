c374183403b5d592664f8f80e1e466cc30e832ae
FilipKainos <filip.szwerluga@kainos.com>
2025-11-20 11:19:57 +0000

    Managed Identify Frontend (#88)

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,3 +2,41 @@ resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
+
+# Data source: Existing Azure Container Registry
+data "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = var.acr_resource_group_name
+}
+
+# Data source: Existing Azure Key Vault
+data "azurerm_key_vault" "kv" {
+  name                = var.key_vault_name
+  resource_group_name = var.key_vault_resource_group_name
+}
+
+# User-Assigned Managed Identity for Container App
+resource "azurerm_user_assigned_identity" "frontend_identity" {
+  name                = var.app_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+
+  tags = {
+    Environment = var.environment
+    Application = var.app_name
+  }
+}
+
+# Role Assignment: Grant AcrPull to Managed Identity
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = data.azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.frontend_identity.principal_id
+}
+
+# Role Assignment: Grant Key Vault Secrets User to Managed Identity
+resource "azurerm_role_assignment" "kv_secrets_user" {
+  scope                = data.azurerm_key_vault.kv.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id         = azurerm_user_assigned_identity.frontend_identity.principal_id
+}
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,47 @@
 variable "resource_group_name" {
   type        = string
   description = "Name of the resource group to create"
-  default     = "team2-fs-test-rg-dev-rg"
+  default     = "team2-job-app-shared-rg"
 }
 
 variable "location" {
   type        = string
   description = "Azure location for resources"
   default     = "UK South"
+}
+
+variable "app_name" {
+  type        = string
+  description = "Name of the application"
+  default     = "team2-job-app"
+}
+
+variable "environment" {
+  type        = string
+  description = "Environment name (e.g., dev, staging, prod)"
+  default     = "dev"
+}
+
+variable "acr_name" {
+  type        = string
+  description = "Name of the existing Azure Container Registry"
+  default     = "aiacademy25"
+}
+
+variable "acr_resource_group_name" {
+  type        = string
+  description = "Resource group name of the existing Azure Container Registry"
+  default     = "container-registry"
+}
+
+variable "key_vault_name" {
+  type        = string
+  description = "Name of the existing Azure Key Vault"
+  default     = "team2-job-app-keyvault"
+}
+
+variable "key_vault_resource_group_name" {
+  type        = string
+  description = "Resource group name of the existing Azure Key Vault"
+  default     = "team2-job-app-shared-rg"
 }
