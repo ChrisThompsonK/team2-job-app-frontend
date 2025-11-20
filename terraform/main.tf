d3b352d7d6f63ad5ff203441b8e87647f91697c6
Filip Szwerluga <filip.szwerluga@kainos.com>
2025-11-20 13:51:32 +0000

    Data sources

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,6 +15,12 @@ data "azurerm_key_vault" "kv" {
   resource_group_name = var.key_vault_resource_group_name
 }
 
+# Data source: Existing Container App Environment
+data "azurerm_container_app_environment" "platform_env" {
+  name                = var.container_app_environment_name
+  resource_group_name = var.container_app_environment_rg
+}
+
 # User-Assigned Managed Identity for Container App
 resource "azurerm_user_assigned_identity" "frontend_identity" {
   name                = var.app_name
@@ -39,4 +45,83 @@ resource "azurerm_role_assignment" "kv_secrets_user" {
   scope                = data.azurerm_key_vault.kv.id
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_user_assigned_identity.frontend_identity.principal_id
+}
+
+# Container App for Frontend
+resource "azurerm_container_app" "frontend" {
+  name                         = "${var.app_name}-app"
+  container_app_environment_id = data.azurerm_container_app_environment.platform_env.id
+  resource_group_name          = azurerm_resource_group.rg.name
+  revision_mode                = "Single"
+
+  identity {
+    type         = "UserAssigned"
+    identity_ids = [azurerm_user_assigned_identity.frontend_identity.id]
+  }
+
+  registry {
+    server   = data.azurerm_container_registry.acr.login_server
+    identity = azurerm_user_assigned_identity.frontend_identity.id
+  }
+
+  template {
+    container {
+      name   = var.app_name
+      image  = "${data.azurerm_container_registry.acr.login_server}/${var.app_name}:${var.container_image_tag}"
+      cpu    = var.container_cpu
+      memory = var.container_memory
+
+      env {
+        name  = "NODE_ENV"
+        value = "production"
+      }
+
+      env {
+        name  = "PORT"
+        value = tostring(var.container_port)
+      }
+
+      env {
+        name  = "BACKEND_API_URL"
+        value = var.backend_api_url
+      }
+
+      env {
+        name        = "SESSION_SECRET"
+        secret_name = "session-secret"
+      }
+    }
+
+    min_replicas = 1
+    max_replicas = 3
+  }
+
+  secret {
+    name                = "session-secret"
+    key_vault_secret_id = "${data.azurerm_key_vault.kv.vault_uri}secrets/session-secret"
+    identity            = azurerm_user_assigned_identity.frontend_identity.id
+  }
+
+  ingress {
+    allow_insecure_connections = false
+    external_enabled           = true
+    target_port                = var.container_port
+    transport                  = "http"
+
+    traffic_weight {
+      latest_revision = true
+      percentage      = 100
+    }
+  }
+
+  tags = {
+    Environment = var.environment
+    Application = var.app_name
+    ManagedBy   = "Terraform"
+  }
+
+  depends_on = [
+    azurerm_role_assignment.acr_pull,
+    azurerm_role_assignment.kv_secrets_user
+  ]
 }
